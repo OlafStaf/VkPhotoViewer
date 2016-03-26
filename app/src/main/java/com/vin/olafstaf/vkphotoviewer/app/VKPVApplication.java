97cec66da108060d434c224e1c392bb4350d82df
Alex Stafiiyevskyi <a.stafiyevsky@gmail.com>
2016-03-26 16:16:00 +0200

    add Crashlytics support.

--- app/src/main/java/com/vin/olafstaf/vkphotoviewer/app/VKPVApplication.java
+++ app/src/main/java/com/vin/olafstaf/vkphotoviewer/app/VKPVApplication.java
@@ -2,8 +2,11 @@ package com.vin.olafstaf.vkphotoviewer.app;
 
 import android.app.Application;
 
+import com.crashlytics.android.Crashlytics;
 import com.vin.olafstaf.vkphotoviewer.app.util.PreferencesManager;
 import com.vk.sdk.VKSdk;
+
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by Stafiiyevskyi on 23.03.2016.
@@ -13,6 +16,7 @@ public class VKPVApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
         VKSdk.initialize(getApplicationContext());
         PreferencesManager.initializeInstance(getApplicationContext());
     }
